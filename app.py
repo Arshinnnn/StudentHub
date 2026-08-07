423f310de46e5121d307144a891cc3873aade2e3
Arshinnnn <adarshrout9090@gmail.com>
2026-08-07 22:58:34 +0530

    Add SQLite database and notes table

--- app.py
+++ app.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/notes")
+def notes():
+    return render_template("notes.html")
+
+@app.route("/planner")
+def planner():
+    return render_template("planner.html")
+
+@app.route("/expenses")
+def expenses():
+    return render_template("expenses.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
